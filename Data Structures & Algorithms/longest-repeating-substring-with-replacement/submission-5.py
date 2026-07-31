a3f10de1da201491bc898ec54fb2122dc42543d8
christianweaver <christianweaver52@gmail.com>
2026-07-31 12:12:58 -0400

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        alph = [0] * 26
+        max_length = 0
+
+        left = 0
+
+        for right in range(len(s)):
+            alph[ord(s[right]) - ord("A")] += 1
+
+            while (right-left+1) - max(alph) > k:
+                alph[ord(s[left]) - ord("A")] -= 1
+                left += 1
+
+            max_length = max(max_length, (right-left+1))
+
+        return max_length
